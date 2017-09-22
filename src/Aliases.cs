26911dd87dcdc5344f5beb1f9df736ebdd1682a0
dwolan@objectivity.co.uk <dwolan@objectivity.co.uk>
2017-09-22 14:33:05 +0200

    GetPublishingCredentialsSlot method added
    RevBy B.H.

--- src/Aliases.cs
+++ src/Aliases.cs
@@ -12,12 +12,25 @@ namespace Cake.Daploy.Azure.Management.WebSites
         [CakeMethodAlias]
         public static User GetPublishingCredentials(this ICakeContext ctx, ServiceClientCredentials credentials, string subscriptionId, string resourceGroupName, string websiteName)
         {
-            WebSiteManagementClient client = new WebSiteManagementClient(credentials)
+            var client = new WebSiteManagementClient(credentials)
             {
                 SubscriptionId = subscriptionId
             };
 
             var retUser = client.WebApps.ListPublishingCredentials(resourceGroupName, websiteName);
+
+            return retUser;
+        }
+
+        [CakeMethodAlias]
+        public static User GetPublishingCredentialsSlot(this ICakeContext ctx, ServiceClientCredentials credentials, string subscriptionId, string resourceGroupName, string websiteName, string slot)
+        {
+            var client = new WebSiteManagementClient(credentials)
+            {
+                SubscriptionId = subscriptionId
+            };
+
+            var retUser = client.WebApps.ListPublishingCredentialsSlot(resourceGroupName, websiteName, slot);
 
             return retUser;
         }
